3a0c25ae14a91322b325deeb0315e8d995a3e639
OzanOcak <ozanocak@gmail.com>
2022-07-07 10:27:14 -0400

    comment show & query

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,6 +4,7 @@ import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 import PortableText from "react-portable-text"; // yarn add
 import { useForm, SubmitHandler } from "react-hook-form"; // yarn add
+import { useState } from "react";
 
 interface Props {
   post: Post;
@@ -18,6 +19,8 @@ interface IFormInput {
 
 function Post({ post }: Props) {
   //console.log(post);
+  const [submitted, setSubmitted] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -32,9 +35,11 @@ function Post({ post }: Props) {
     })
       .then(() => {
         console.log(data);
+        setSubmitted(true);
       })
       .catch((err) => {
         console.log(err);
+        setSubmitted(false);
       });
   };
 
@@ -93,60 +98,71 @@ function Post({ post }: Props) {
 
         <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
-        >
-          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
-          <hr className="mt-2 py-3" />
-          <input
-            type="hidden"
-            {...register("_id")}
-            name="_id"
-            value={post._id}
-          />
-          <label className="mb-5 block">
-            <span className="text-gray-700">Name</span>
-            <input
-              {...register("name", { required: true })}
-              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
-              placeholder="John Appleseed"
-              type="text"
-            />
-          </label>
-          <label className="mb-5 block">
-            <span className="text-gray-780">Email</span>
-            <input
-              {...register("email", { required: true })}
-              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
-              placeholder="John Appleseed"
-              type="text"
-            />
-          </label>
-
-          <label className="mb-5 block">
-            <span className="text-gray-700">Comment</span>
-            <textarea
-              {...register("comment", { required: true })}
-              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
-              placeholder="John Appleseed"
-              rows={8}
-            />
-          </label>
-          <div className="flex flex-col p-5">
-            {errors.name && <p className="text-red-500">Name is required</p>}
-            {errors.email && <p className="text-red-500">Email is required</p>}
-            {errors.comment && (
-              <p className="text-red-500">Comment is required</p>
-            )}
+        {submitted ? (
+          <div className=" flex flex-col py-10 my-10 bg-yellow-500 text-white">
+            <h3 className="text-3xl font-bold">
+              Thank you for submitting you comment!
+            </h3>
+            <p>Once it has been approwed, it will appear below!</p>
           </div>
+        ) : (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+          >
+            <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+            <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+            <hr className="mt-2 py-3" />
+            <input
+              type="hidden"
+              {...register("_id")}
+              name="_id"
+              value={post._id}
+            />
+            <label className="mb-5 block">
+              <span className="text-gray-700">Name</span>
+              <input
+                {...register("name", { required: true })}
+                className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
+                placeholder="John Appleseed"
+                type="text"
+              />
+            </label>
+            <label className="mb-5 block">
+              <span className="text-gray-780">Email</span>
+              <input
+                {...register("email", { required: true })}
+                className="form-input mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
+                placeholder="John Appleseed"
+                type="text"
+              />
+            </label>
 
-          <input
-            type="submit"
-            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
-          />
-        </form>
+            <label className="mb-5 block">
+              <span className="text-gray-700">Comment</span>
+              <textarea
+                {...register("comment", { required: true })}
+                className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow ring-yellow-500 outline-none focus:ring"
+                placeholder="John Appleseed"
+                rows={8}
+              />
+            </label>
+            <div className="flex flex-col p-5">
+              {errors.name && <p className="text-red-500">Name is required</p>}
+              {errors.email && (
+                <p className="text-red-500">Email is required</p>
+              )}
+              {errors.comment && (
+                <p className="text-red-500">Comment is required</p>
+              )}
+            </div>
+
+            <input
+              type="submit"
+              className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+            />
+          </form>
+        )}
       </main>
     </>
   );
--- typings.d.ts
+++ typings.d.ts
@@ -18,3 +18,19 @@ export interface Post {
   };
   body: [object]; // body: array of objects
 }
+
+export interface Comment {
+  approved: boolean;
+  comment: string;
+  email: string;
+  name: string;
+  post: {
+    _ref: string;
+    _type: string;
+  };
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+}
